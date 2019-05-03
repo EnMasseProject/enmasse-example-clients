643816a6bf9074fca9181c307fd807183ba5e187
Ulf Lilleengen <lulf@redhat.com>
2019-05-03 12:59:42 +0200

    Update

--- microprofile-example-client/src/main/java/io/enmasse/example/microprofile/Main.java
+++ microprofile-example-client/src/main/java/io/enmasse/example/microprofile/Main.java
@@ -14,25 +14,14 @@ import java.util.concurrent.CompletionStage;
 @ApplicationScoped
 public class Main {
 
+    private int count = 0;
+
     @Incoming("orders")
     @Outgoing("confirmations")
-    public Message<String> process(Message<String> event) {
+    public Message<String> process(Message<String> event) throws InterruptedException {
+        Thread.sleep(1000);
         return Message.of("Thanks for the order: " + event.getPayload() + ". Now go do something useful");
     }
-
-    @Outgoing("orders")
-    public PublisherBuilder<Message<String>> source() {
-        return ReactiveStreams.of(Message.of("hello"), Message.of("with"), Message.of("SmallRye"), Message.of("reactive"), Message.of("message"));
-    }
-
-    @Incoming("confirmations")
-    public CompletionStage<Integer> sink(Message<String> event) {
-        System.out.println("Sink: " + event.getPayload());
-        CompletableFuture<Integer> promise = new CompletableFuture<>();
-        promise.complete(1);
-        return promise;
-    }
-
 
     public static void main(String[] args) {
         SeContainerInitializer.newInstance().initialize();
